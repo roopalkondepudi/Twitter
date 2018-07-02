25f8c2aaf2c21c4ac25c6228b92a0ecdf3bf775b
Roopal Kondepudi <roopalk@fb.com>
2018-07-02 15:12:03 -0700

    trying to make the compose functionality for Twitter

---
+++ FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,35 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvTweet) TextView tvTweet;
+    @BindView(R.id.tweetCompose) Button tweetCompose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+        ButterKnife.bind(this);
+    }
+
+    public void onClick(View v)
+    {
+        Toast.makeText(ComposeActivity.this, "Success", Toast.LENGTH_SHORT).show();
+    }
+}
