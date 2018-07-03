222543053c40ca1aaaac75d7295eb6abe1a9ff79
Roopal Kondepudi <roopalk@fb.com>
2018-07-02 20:55:15 -0700

    added a timestamp for the user to see when the tweets were tweeted

--- FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,10 +1,12 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +19,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -65,17 +71,23 @@ public class TimelineActivity extends AppCompatActivity {
     //on options item selected
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        switch(menuItem.getItemId()) //handles all the menu options
+        {
+            case R.id.tweetCompose:
+                Intent intent = new Intent(TimelineActivity.this, ComposeActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 
-    private void populateTimeline()
-    {
-        client.getHomeTimeline(new JsonHttpResponseHandler()
-        {
+    private void populateTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response)
-            {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 Log.d("TwitterClient", response.toString()); //logging what's happening
             }
 
@@ -83,13 +95,12 @@ public class TimelineActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 Log.d("TwitterClient", response.toString());
                 //for each entry, deserialize each JSON entry
-                for(int i = 0; i < response.length(); i++)
-                {
+                for (int i = 0; i < response.length(); i++) {
                     //Tweet tweet = null;
                     try {
                         Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
                         tweets.add(tweet);
-                        tweetAdapter.notifyItemInserted(tweets.size()-1);
+                        tweetAdapter.notifyItemInserted(tweets.size() - 1);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -115,5 +126,41 @@ public class TimelineActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    //add the timestamp to every tweet displayed
+    public String getTimeStamp(String dateInJSON)
+    {
+        String twitterDateFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        /*SimpleDateFormat - a class for formatting and parsing dates in a locale-sensitive manner
+         * EEE - day in week (letter I'm guessing, e.g. MON)
+         * MMM - month in year (also letter, e.g. MAY)
+         * dd/HH/mm/ss pretty straightforward time stuff
+         * ZZZZZ - time zone, although I'm not sure why there are 5
+         * yyyy - year
+         */
+        SimpleDateFormat time = new SimpleDateFormat(twitterDateFormat, Locale.ENGLISH);
+        /* Locale - object that represents a specific geographical/political/cultural region
+         * Locale(String language, String country) OR Locale(String country), the latter of which we are using in this context
+         * I could easily say English, US
+         * Country and Language is used with ISO codes
+         */
+
+        /*setLenient the time parsing might be lenient*/
+        time.setLenient(true);
+
+        String timeStamp = "";
+        try
+        {
+            long dateInMilliseconds = time.parse(dateInJSON).getTime();
+            timeStamp = DateUtils.getRelativeTimeSpanString(dateInMilliseconds, System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+        return timeStamp;
     }
 }
