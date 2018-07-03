9e1679d0f085d9170a1a79e80497abd83370c870
Roopal Kondepudi <roopalk@fb.com>
2018-07-03 15:40:59 -0700

    modified compose activity to see if swipe view was possible

--- FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,27 +1,19 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.Typeface;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcels;
-
-import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -45,7 +37,7 @@ public class ComposeActivity extends AppCompatActivity {
         client = TwitterApp.getRestClient(this);
     }
 
-    public void onClick(View v)
+    public void onSubmit(View v)
     {
         // Toast.makeText(ComposeActivity.this, "Success", Toast.LENGTH_SHORT).show();
         message = tvTweet.getText().toString(); // get the message
@@ -60,7 +52,8 @@ public class ComposeActivity extends AppCompatActivity {
                     //send back the new tweet as a result back to the original activity
                     Intent intent = new Intent(ComposeActivity.this, TimelineActivity.class);
                     intent.putExtra("myTweet", Parcels.wrap(tweet));
-                    getBaseContext().startActivity(intent);
+                    setResult(RESULT_OK, intent);
+                    finish();
                 }
                 catch (JSONException e) {
                     e.printStackTrace();
