7989a439860d14684a5f845e081de291035c489d
Roopal Kondepudi <roopalk@fb.com>
2018-07-06 15:54:55 -0700

    started adding images but gave up for now

--- FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ FBUTwitterClient/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -76,7 +76,6 @@ public class TimelineActivity extends AppCompatActivity {
         });
 
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
-
     }
 
     public void fetchTimelineAsync(int page) {
